57fc4b38d639a9b52684e787b5c7f01dd299eb6a
GL <674057373@qq.com>
2016-06-23 22:41:24 +0800

    【开始使用FMDB数据库进行缓存】

---
+++ revaluation_Bili/revaluation_Bili/Other/DataToolSDK/GLFMDBToolSDK.h
@@ -0,0 +1,18 @@
+//
+//  GLFMDBToolSDK.h
+//  revaluation_Bili
+//
+//  Created by mac on 16/6/23.
+//  Copyright © 2016年 mac. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+/** 导入FMDB框架
+    封装工具类 【使用队列-存储在cache文件夹】,提供 CURD接口
+    单例模式
+ 
+ */
+
+@interface GLFMDBToolSDK : NSObject
+
+@end
---
+++ revaluation_Bili/revaluation_Bili/Other/DataToolSDK/GLFMDBToolSDK.m
@@ -0,0 +1,13 @@
+//
+//  GLFMDBToolSDK.m
+//  revaluation_Bili
+//
+//  Created by mac on 16/6/23.
+//  Copyright © 2016年 mac. All rights reserved.
+//
+
+#import "GLFMDBToolSDK.h"
+
+@implementation GLFMDBToolSDK
+
+@end
