7ce453fdc0ce055b7c0527779226c45e65c4841a
GL <674057373@qq.com>
2016-07-05 23:58:22 +0800

    【下载管理界面搭建】

---
+++ revaluation_Bili/revaluation_Bili/Classes/6VideoRoom(AV9999999)/ViewController/GLVideoDownloadMViewController.h
@@ -0,0 +1,13 @@
+//
+//  GLVideoDownloadMViewController.h
+//  revaluation_Bili
+//
+//  Created by mac on 16/7/5.
+//  Copyright © 2016年 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GLVideoDownloadMViewController : UIViewController
+
+@end
